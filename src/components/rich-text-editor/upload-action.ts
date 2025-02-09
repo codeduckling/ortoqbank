5ee2c84c1ba35dd6c336273c594ec5e537d9866d
Patolord <rodrigo_luizp@hotmail.com>
2025-02-09 19:48:28 -0300

    Update ImageKit configuration environment variables

--- src/components/rich-text-editor/upload-action.ts
+++ src/components/rich-text-editor/upload-action.ts
@@ -3,9 +3,9 @@
 import ImageKit from 'imagekit';
 
 const imageKit = new ImageKit({
-  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
-  privateKey: process.env.PRIVATE_KEY!,
-  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
 });
 
 export async function uploadToImageKit(file: File) {
