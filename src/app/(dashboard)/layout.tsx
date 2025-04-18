58fc71d44f48b4a57d1c3a20986f1b76eb54a931
Patolord <rodrigo_luizp@hotmail.com>
2025-04-17 22:35:33 -0300

    feat: Implement role-based layout adjustments for admin users
    - Added role checking functionality to the Layout component to conditionally apply CSS classes based on admin status.
    - Updated main class name to allow for different styling when the user is not an admin, enhancing user experience.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,24 @@
 import AccessCheck from '@/components/AccessCheck';
 import { AppSidebar } from '@/components/sidebar/app-sidebar';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { checkRole } from '@/utils/roles'; // Adjust path if necessary
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const isAdmin = await checkRole('admin');
+
+  const mainClassName = `w-full ${isAdmin ? '' : 'select-none'}`;
+
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full">
+      {/* Apply conditional class */}
+      <main className={mainClassName.trim()}>
+        {' '}
+        {/* Use trim to remove trailing space if isAdmin */}
         <SidebarTrigger />
         <div className="mx-auto max-w-5xl px-4">
           <AccessCheck>{children}</AccessCheck>
