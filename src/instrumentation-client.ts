8584a77a1af00c2690279d43d4151828f7c10333
Patolord <rodrigo_luizp@hotmail.com>
2025-04-21 21:24:37 -0300

    chore: Update Sentry configuration and dependencies
    - Added `.sentryclirc` to `.gitignore` for Sentry configuration management.
    - Updated ESLint configuration to disable the 'unicorn/no-abusive-eslint-disable' rule.
    - Upgraded `@sentry/cli` to version 2.43.0 in `package.json` and `package-lock.json`.
    - Added a new script in `package.json` for Sentry sourcemaps integration during the build process.

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,16 +1,16 @@
+/* eslint-disable */
+
 // This file configures the initialization of Sentry on the client.
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: "https://0664a19614699bd6b967054f2a0e4820@o4509164953600000.ingest.us.sentry.io/4509194001317888",
+  dsn: 'https://0664a19614699bd6b967054f2a0e4820@o4509164953600000.ingest.us.sentry.io/4509194001317888',
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -21,7 +21,7 @@ Sentry.init({
   replaysSessionSampleRate: 0.1,
 
   // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
